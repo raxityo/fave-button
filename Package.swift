640c064dee7bcb75e2f1f94dd709d2bb16127ef3
Raxit Majithiya <rax@expa.com>
2020-08-04 11:08:25 -0400

    Revert "Exclude FaveButtonDemo"
    This reverts commit 8be1a35f0c4300ec8572d6c0c5b3de60f0da2138.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
   name: "FaveButton",
-  exclude: ["FaveButtonDemo"],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(
